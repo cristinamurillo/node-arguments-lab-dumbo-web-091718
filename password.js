4df3ea7ec325d0384bc721706c6a317187f727e9
Cristina Murillo <cristinamurillo@Cristinas-Air.home>
2019-02-01 18:51:38 -0500

    finished lab

--- password.js
+++ password.js
@@ -1,1 +1,22 @@
 // code solution here
+process.argv.shift()
+process.argv.shift()
+
+function printPasswords(){
+    for(i=0;i<process.argv[1];i++){
+        generatePassword(process.argv[0])
+    }
+}
+
+function generatePassword(length){
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    let password = ""
+    for(let i=0; i<length;i++){
+        password += charset.charAt(Math.floor(Math.random()*charset.length))
+    }
+    process.stdout.write(password)
+}
+
+printPasswords()
+
+process.exit(0)
